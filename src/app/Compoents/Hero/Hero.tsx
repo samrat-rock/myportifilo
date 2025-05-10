78dc169b260236219599740f2ebee72a1ffc86b6
samrat-rock <samratpandey715@gmail.com>
2025-05-10 22:30:36 +0545

    change ,ade on navbar

--- src/app/Compoents/Hero/Hero.tsx
+++ src/app/Compoents/Hero/Hero.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='p-10'>
+     <div className='bg-yellow-300 border-2 w-4 h-4'></div>
+    </div>
   )
 }
 
